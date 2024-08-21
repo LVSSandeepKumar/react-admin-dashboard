f544e2129406275ea347320de5b22cc7bd207d2f
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-21 13:01:58 +0530

    Routing setup with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
-
   return (
-    <div className="text-red-400">
-      It's a new day
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
