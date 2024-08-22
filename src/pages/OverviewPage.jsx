91b2cd30ec9a2a1974f4d84141eacd63a431bb34
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-22 18:47:25 +0530

    Products Page UI

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -14,7 +14,7 @@ const OverviewPage = () => {
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         <motion.div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-8"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-8 gap-8"
         initial={{opacity: 0, y:20}}
         animate={{opacity: 1, y:0}}
         transition={{duration: 1}}        >
