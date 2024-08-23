2282ef5a93b2570de9888fe3119a87627397a2ff
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-23 09:17:55 +0530

    Users Page UI

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,68 @@
+import { UserCheck, UserPlus, UsersIcon, UserX } from "lucide-react";
+import {motion} from "framer-motion";
+
+import StatCard from "../components/common/StatCard";
+import Header from "../components/common/Header";
+import UsersTable from "../components/users/UsersTable";
+import UsersGrowthChart from "../components/users/UsersGrowthChart";
+import UsersActivityHeatmap from "../components/users/UsersActivityHeatmap";
+import UsersDemographicsChart from "../components/users/UsersDemographicsChart";
+
+const userStats = {
+  totalUsers: 152845,
+  newUsersToday: 243,
+  activeUsers: 98520,
+  churnRate: "2.4%",
+};
+
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
-  )
-}
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Users" />
 
-export default UsersPage
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STATS */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Users"
+            icon={UsersIcon}
+            value={userStats.totalUsers.toLocaleString()}
+            color="#6366F1"
+          />
+          <StatCard
+            name="New Users Today"
+            icon={UserPlus}
+            value={userStats.newUsersToday}
+            color="#10B981"
+          />
+          <StatCard
+            name="Active Users"
+            icon={UserCheck}
+            value={userStats.activeUsers.toLocaleString()}
+            color="#F59E0B"
+          />
+          <StatCard
+            name="Churn Rate"
+            icon={UserX}
+            value={userStats.churnRate}
+            color="#EF4444"
+          />
+        </motion.div>
+        <UsersTable />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 mt-8 gap-8">
+          <UsersGrowthChart />
+          <UsersActivityHeatmap />
+          <UsersDemographicsChart />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default UsersPage;
